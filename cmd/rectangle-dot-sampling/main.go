50abc1a64a20053cd0ab5c11051e3cdd2c5d33bb
Devin McGloin <devin.mcgloin@intercom.io>
2018-12-20 14:26:08 -0800

    Iterating on different approaches

--- cmd/rectangle-dot-sampling/main.go
+++ cmd/rectangle-dot-sampling/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"image/color"
+	"math"
+	"math/rand"
+	"time"
 
 	"github.com/devinmcgloin/clr/clr"
 	"github.com/devinmcgloin/generative-go/pkg/canvas"
@@ -12,10 +15,15 @@ import (
 )
 
 func main() {
+	seed := time.Now().Unix()
 	context := gg.NewContext(1400, 900)
-	var rows = 10.0
-	var margin = 0.05
-	filler := fill.NewUniformFiller(8000, 42)
+	r := rand.New(rand.NewSource(seed))
+	rows := 1 + math.Floor(r.Float64()*15)
+	margin := r.Float64() * 0.10
+	hue := uint16(r.Intn(365))
+
+	fmt.Printf("Seed: %d Rows: %f Margin: %f Hue: %d\n", seed, rows, margin, hue)
+	filler := fill.NewUniformFiller(8000, seed)
 	context.SetColor(color.White)
 	context.DrawRectangle(0, 0, 1400, 900)
 	context.Fill()
@@ -32,16 +40,18 @@ func main() {
 			},
 		}
 
-		r, g, b := clr.HSV{H: 34, S: uint8(i * 7), V: 70}.RGB()
+		r, g, b := clr.HSV{H: hue, S: uint8(i * 7), V: 70}.RGB()
 		context.SetRGB(float64(r), float64(g), float64(b))
 
-		fmt.Printf("%+v R: %d, G: %d, B: %d\n", rect, r, g, b)
 		filler.DotFill(context, rect)
 	}
-	context.SavePNG("out.png")
+	err := context.SavePNG(fmt.Sprintf("./sketches/rectangle-sampling/%d-sketch.png", filler.Seed))
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 func rectangePositioning(offset, index, rectangeCount float64) float64 {
 	avaliableSpace := 1.0 - offset*2
-	return offset + index*avaliableSpace/rectangeCount
+	return offset + index*(avaliableSpace/rectangeCount)
 }
